0ca430cfb317effbe2fe5b790055ec42b25bc5f3
young-win <young.zhu.2012@gmail.com>
2023-11-30 14:53:48 +0800

    RefreshToken()

--- basecamp/basecamp.go
+++ basecamp/basecamp.go
@@ -1,7 +1,10 @@
 // Package basecamp provides constants for using OAuth2 to access the Basecamp API.
 package basecamp // import "github.com/youngzhu/oauth2-apps/basecamp"
 
-import "golang.org/x/oauth2"
+import (
+	"context"
+	"golang.org/x/oauth2"
+)
 
 const (
 	authURL  = "https://launchpad.37signals.com/authorization/new"
@@ -16,4 +19,22 @@ var Endpoint = oauth2.Endpoint{
 var Endpoint4Refresh = oauth2.Endpoint{
 	AuthURL:  authURL,
 	TokenURL: tokenURL + "?type=refresh",
+}
+
+// RefreshToken use to request a new access token when it expires (2 week lifetime, currently).
+// see https://github.com/basecamp/api/blob/master/sections/authentication.md#get-authorization
+// refreshToken - must be refreshToken, NOT the accessToken
+func RefreshToken(clientID, clientSecret, refreshToken string) error {
+	conf := &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     Endpoint4Refresh,
+		// 根据各网站的API的权限范围设置scopes
+		// 不确定时，可以不设置
+		Scopes: []string{""},
+	}
+
+	_, err := conf.TokenSource(context.Background(), &oauth2.Token{RefreshToken: refreshToken}).Token()
+
+	return err
 }
---
+++ basecamp/basecamp_test.go
@@ -0,0 +1,34 @@
+package basecamp
+
+import (
+	"github.com/spf13/viper"
+	"testing"
+)
+
+func init() {
+	viper.SetEnvPrefix("BASECAMP")
+	viper.AutomaticEnv() // read in environment variables that match
+}
+
+func TestRefreshToken(t *testing.T) {
+	clientID := viper.GetString("clientID")
+	clientSecret := viper.GetString("clientSecret")
+	refreshToken := viper.GetString("REFRESH_TOKEN")
+
+	if clientID == "" {
+		t.Error("clientID should not empty")
+	}
+
+	if clientSecret == "" {
+		t.Error("clientSecret should not empty")
+	}
+
+	if refreshToken == "" {
+		t.Error("refreshToken should not empty")
+	}
+
+	err := RefreshToken(clientID, clientSecret, refreshToken)
+	if err != nil {
+		t.Error(err)
+	}
+}
